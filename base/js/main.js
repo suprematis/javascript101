22c25c55e09a7345d9bc2d6f34f2831898c50d35
Andres Salgado <as+github@andresalgado.com>
2018-03-20 11:37:41 -0700

    Finished up to middle of video #6

--- base/js/main.js
+++ base/js/main.js
@@ -1,2 +1,27 @@
+/* This
+is a
+multiline
+comment
+*/
 var hello = "Hello World";
 console.log(hello)
+
+// Sets value
+var value = 3;
+var valueTwo = "3";
+
+// Boolean test
+var open = true;
+console.log(open);
+console.log(value + 3);
+// This adds valueTwo + the character "3"
+console.log(valueTwo + "3");
+console.log(value == 3);
+
+// if statement basics
+var name = "Andres";
+// Checks to see if name is Andres
+if (name === "Andres"){
+  //Output something
+    console.log(name + " is the name")
+}
